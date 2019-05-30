e18e94b0788a7322ea308e232c115eddadbce3c2
mulwatech <mulwatech@gmail.com>
2019-05-30 16:32:55 +0300

    adds user model

---
+++ src/Models/userModel.ts
@@ -0,0 +1,18 @@
+import * as mongoose from 'mongoose'
+
+const  Schema = mongoose.Schema;
+
+export const UserSchema = new Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    emailAddress: {
+        type: String,
+        required: true,
+    }
+})
